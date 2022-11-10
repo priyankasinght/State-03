e4e1f40e560b2ba1dc5408ae205caf974db00c5b
priyankasinght <35567382+priyankasinght@users.noreply.github.com>
2022-11-10 16:55:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React, { useState } from 'react'
+import './App.css'
+import {
+  numbers,
+  upperCaseLetters,
+  lowerCaseLetters,
+  specialCharacters,
+} from './characters'
+
+function App() {
+  const [password, setPassword] = useState('')
+  const [passwordLength, setPasswordLength] = useState(20)
+  const [includeUppercase, setIncludeUppercase] = useState(false)
+  const [includeLowercase, setIncludeLowercase] = useState(false)
+  const [includeNumbers, setIncludeNumbers] = useState(false)
+  const [includeSymbols, setIncludeSymbols] = useState(false)
+
+  const handleGeneratePassword = (e) => {
+    
+    let characterList = ''
+
+    if (includeLowercase) {
+      characterList = characterList + lowerCaseLetters
+    }
+
+    if (includeUppercase) {
+      characterList = characterList + upperCaseLetters
+    }
+
+    if (includeNumbers) {
+      characterList = characterList + numbers
+    }
+
+    if (includeSymbols) {
+      characterList = characterList + specialCharacters
+    }
+
+    setPassword(createPassword(characterList))
+  }
+  
+  const createPassword = (characterList) => {
+    let password = ''
+    const characterListLength = characterList.length
+
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characterListLength)
+      password = password + characterList.charAt(characterIndex)
+    }
+    return password
+  }
+  
+  return (
+    <div className='App'>
+      <div className='container'>
+        <div className='generator'>
+          <h2 className='generator__header'>Password Generator</h2>
+          <div className='generator__password'>
+            <h3>{password}</h3>
+            <button  className='copy__btn'>
+              <i className='far fa-clipboard'></i>
+            </button>
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='password-strength'>Select Password length</label>
+            <input
+              defaultValue={passwordLength}
+              onChange={(e) => setPasswordLength(e.target.value)}
+              type='number'
+              id='password-strength'
+              name='password-strength'
+              max='20'
+              min='8'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='uppercase-letters'>Include Uppercase Letters</label>
+            <input
+              checked={includeUppercase}
+              onChange={(e) => setIncludeUppercase(e.target.checked)}
+              type='checkbox'
+              id='uppercase-letters'
+              name='uppercase-letters'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='lowercase-letters'>Include Lowercase Letters</label>
+            <input
+              checked={includeLowercase}
+              onChange={(e) => setIncludeLowercase(e.target.checked)}
+              type='checkbox'
+              id='lowercase-letters'
+              name='lowercase-letters'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='include-numbers'>Include Numbers</label>
+            <input
+              checked={includeNumbers}
+              onChange={(e) => setIncludeNumbers(e.target.checked)}
+              type='checkbox'
+              id='include-numbers'
+              name='include-numbers'
+            />
+          </div>
+
+          <div className='form-group'>
+            <label htmlFor='include-symbols'>Include Symbols</label>
+            <input
+              checked={includeSymbols}
+              onChange={(e) => setIncludeSymbols(e.target.checked)}
+              type='checkbox'
+              id='include-symbols'
+              name='include-symbols'
+            />
+          </div>
+
+          <button onClick={handleGeneratePassword} className='generator__btn'>
+            Generate Password
+          </button>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App;
